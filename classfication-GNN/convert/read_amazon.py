94d4ed514d7b775e6c5f280c801c50eae82ef12a
KamalovMikhail <mkamalovv@gmail.com>
2022-05-02 22:06:35 +0530

    Update read_amazon.py
    append name for dataset path

--- classfication-GNN/convert/read_amazon.py
+++ classfication-GNN/convert/read_amazon.py
@@ -16,32 +16,10 @@ from sparsegraph import SparseGraph
 import struct
 from sklearn.preprocessing import StandardScaler
 
-def load_graph():
-    dataset_path = '/home/user/proj/AGP/classfication-GNN/data/amazon'
-    adj_matrix = scipy.sparse.load_npz('{}/adj_full.npz'.format(dataset_path))
+ 
 
-    attr_matrix = np.load('{}/feats.npy'.format(dataset_path))
-    class_map = json.load(open('{}/class_map.json'.format(dataset_path)))
-    class_map = {int(k): v for k, v in class_map.items()}
-    num_vertices = adj_matrix.shape[0]
-    if isinstance(list(class_map.values())[0], list):
-        num_classes = len(list(class_map.values())[0])
-        class_arr = np.zeros((num_vertices, num_classes))
-        for k, v in class_map.items():
-            class_arr[k] = v
-    else:
-        num_classes = max(class_map.values()) - min(class_map.values()) + 1
-        class_arr = np.zeros((num_vertices, num_classes))
-        offset = min(class_map.values())
-        for k, v in class_map.items():
-            class_arr[k][v - offset] = 1
-    # num_classes = class_arr.shape[1]
-    class_arr = np.argmax(class_arr, axis=-1)
-    g = SparseGraph(adj_matrix=adj_matrix, attr_matrix=attr_matrix, labels=class_arr)
-    return g
-
-def load_data(ntrain_div_classes, seed=0):
-    dataset_path = '/home/user/proj/AGP/classfication-GNN/data/amazon'
+def load_data(ntrain_div_classes, name, seed=0):
+    dataset_path = f'/home/test/Documents/genpr_master/genpr/gbsa_appnp/data/{name}'
     adj_matrix = scipy.sparse.load_npz('{}/adj_full.npz'.format(dataset_path))
 
     attr_matrix = np.load('{}/feats.npy'.format(dataset_path))
